43c7da5051b8521a212f1d77f74f10463ac849bd
b-balakrishna <battulabalakrishna063@gmail.com>
2024-06-02 01:50:30 +0530

    Binary Search Tree

---
+++ BinarySearchTree.java
@@ -0,0 +1,59 @@
+public class BinarySearchTree {
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public Node getRoot() {
+        return this.root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (this.root == null) {
+            this.root = newNode;
+            return true;
+        }
+        Node temp = this.root;
+        while (true) {
+            if (newNode.value == temp.value)
+                return false;
+
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        Node temp = this.root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
